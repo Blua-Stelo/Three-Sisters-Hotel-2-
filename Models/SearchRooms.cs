aa7300caacd96dd9e2b706dce13e329d75c41a82
james fu <fuzihaoooo@gmail.com>
2021-02-01 16:44:08 +1100

    search model

---
+++ Models/SearchRooms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Three_Sisters_Hotel.Models
+{
+    public class SearchRooms
+    {
+        [Required]
+        public int Beds { get; set; }
+        [Required]
+        public DateTime Checkin { get; set; }
+        [Required]
+        public DateTime Checkout { get; set; }
+    }
+}
