2f33dbcec76eac300255a2f13094ae692acb1221
james fu <fuzihaoooo@gmail.com>
2021-02-01 19:38:57 +1100

    book a room

--- Pages/Bookings/Create.cshtml.cs
+++ Pages/Bookings/Create.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Three_Sisters_Hotel.Data;
 using Three_Sisters_Hotel.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Three_Sisters_Hotel.Pages.Bookings
 {
@@ -27,21 +30,43 @@ namespace Three_Sisters_Hotel.Pages.Bookings
         }
 
         [BindProperty]
-        public Booking Booking { get; set; }
+        public Booking Bookings { get; set; }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            string _email = User.FindFirst(ClaimTypes.Name).Value;
+
+            Booking bookings = await _context.Booking.FirstOrDefaultAsync(m => m.CustomerEmail == _email);
+
             if (!ModelState.IsValid)
             {
+                ViewData["CustomerEmail"] = new SelectList(_context.Set<Customer>(), "Email", "Email");
+                ViewData["RoomID"] = new SelectList(_context.Set<Room>(), "ID", "ID");
                 return Page();
             }
 
-            _context.Booking.Add(Booking);
+            bookings = new Booking();
+            bookings.RoomID = Bookings.RoomID;
+            bookings.CustomerEmail = _email;
+            bookings.ChecnIn = Bookings.ChecnIn;
+            bookings.CheckOut = Bookings.CheckOut;
+            var totalday = (bookings.CheckOut - bookings.ChecnIn).Days;
+            // retrieve the pizza to get its price
+            var theRoom = await _context.Room.FindAsync(Bookings.RoomID);
+            // calculate the total price of this order
+            bookings.Cost = (int)totalday * theRoom.Price;
+
+            _context.Booking.Add(bookings);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            ViewData["pizza"] = theRoom.ID;
+            ViewData["total"] = bookings.Cost;
+            ViewData["SuccessDB"] = "success";
+            
+
+            return Page();
         }
     }
 }
